680622c4ace6619580e4c11ec28d00e85065ca7c
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-08 12:56:04 +0100

    Add files via upload

---
+++ src/main/java/com/example/eiffelskills_back/EiffelskillsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.eiffelskills_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiffelskillsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiffelskillsBackApplication.class, args);
+	}
+
+}
