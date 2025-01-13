5d00ea1e5079423109239792c4e18c16e49166b7
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-13 15:31:05 +0100

    Debug.

--- initdb/insertdata.sql
+++ initdb/insertdata.sql
@@ -1,2 +1,2 @@
 -- insert admin user
-insert into public.users(name,mail,password,role) values('admin','admin@es.fr','743139240ff612253817440d98acb2ce7939fbb4','admin');
+insert into public.users(name,mail,password,role) values('admin','admin@es.fr','[B@374d1c30','admin');
