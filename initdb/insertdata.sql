f3702504ebcf977a5868cb51d8ae3fcdefe84806
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-13 14:56:22 +0100

    Change admin pwd hash.

--- initdb/insertdata.sql
+++ initdb/insertdata.sql
@@ -1,2 +1,2 @@
 -- insert admin user
-insert into public.users(name,mail,password,role) values('admin','admin@es.fr','$2a$12$tu5xQgM2LPaEUU/.PPJWxOaz2v./DImPwhfBHQnBcFW82axEAEo3S','admin');
+insert into public.users(name,mail,password,role) values('admin','admin@es.fr','743139240ff612253817440d98acb2ce7939fbb4','admin');
