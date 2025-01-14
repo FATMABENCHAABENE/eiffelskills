f8821a695893681f3472066770d4ab9214e09097
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-14 17:42:20 +0100

    Minor modification.

--- src/main/java/com/example/eiffelskills_back/DAO/AutoEvaluationDAO.java
+++ src/main/java/com/example/eiffelskills_back/DAO/AutoEvaluationDAO.java
@@ -7,12 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface AutoEvaluationDAO extends JpaRepository<AutoEvaluations, Long> {
-    @Query(nativeQuery = true, value = "UPDATE autoevaluation SET id_skill=:idSkill, id_student=:idStudent, eval=:eval WHERE id=:id")
+    @Query(nativeQuery = true, value = "UPDATE autoevaluations SET id_skill=:idSkill, id_student=:idStudent, eval=:eval WHERE id=:id")
     public void updateAutoEvaluationById(Long id, Long idSkill, Long idStudent, String eval);
 
-    @Query(nativeQuery = true, value = "UPDATE autoevaluation SET eval=:eval WHERE id_skill=:idSkill and id_student=:idStudent")
+    @Query(nativeQuery = true, value = "UPDATE autoevaluations SET eval=:eval WHERE id_skill=:idSkill and id_student=:idStudent")
     public void updateAutoEvaluationBySkillAndStudent(Long idSkill, Long idStudent, String eval);
 
-    @Query(nativeQuery = true, value = "SELECT * FROM autoevaluation WHERE id_skill=:idSkill and id_student=:idStudent")
+    @Query(nativeQuery = true, value = "SELECT * FROM autoevaluations WHERE id_skill=:idSkill and id_student=:idStudent")
     public Optional<AutoEvaluations> findBySkillAndStudent(Long idSkill, Long idStudent);
 }
