7860cfe2216726eca42c936141f672d24860ea04
qentex4 <quentinlacombe464@yahoo.fr>
2025-01-14 13:52:18 +0100

    navbar + Connexion

--- src/app/fonction/fonction.component.ts
+++ src/app/fonction/fonction.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class FonctionComponent implements OnInit {
   mail: string = "";
   password: string = "";
 
