0a181cc9d547797dc671f608c06133196af2f984
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-16 14:59:09 +0100

    Add Questions.java .

--- src/main/java/com/example/eiffelskills_back/models/Questions.java
+++ src/main/java/com/example/eiffelskills_back/models/Questions.java
@@ -1,8 +1,26 @@
 package com.example.eiffelskills_back.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-
+@Getter
+@Setter
+@Table(name = "questions")
 public class Questions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "id_mcq")
+    private Long idMcq;
+
+    public Questions() {}
+
+    public Questions(String description, Long idMcq) {
+        this.description = description;
+        this.idMcq = idMcq;
+    }
 }
