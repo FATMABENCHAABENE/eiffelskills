26e960da5d228476a7c1acd7ac0cf2c54d65eae7
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-16 15:10:16 +0100

    Minor modification.

--- src/main/java/com/example/eiffelskills_back/services/McqService.java
+++ src/main/java/com/example/eiffelskills_back/services/McqService.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class McqService {
-    private McqDAO mcqDAO;
+    private final McqDAO mcqDAO;
 
     @Transactional
     public Mcq saveMcq(Mcq mcq) {
