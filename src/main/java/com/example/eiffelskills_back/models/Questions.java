e64c56f5a2f52e200d0797e037ca269ac3556c7e
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-17 14:47:27 +0100

    Add idSkill in Question .

--- src/main/java/com/example/eiffelskills_back/models/Questions.java
+++ src/main/java/com/example/eiffelskills_back/models/Questions.java
@@ -12,15 +12,18 @@ public class Questions {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "description")
+    @Column(name = "description", nullable = false)
     private String description;
-    @Column(name = "id_mcq")
+    @Column(name = "id_mcq", nullable = false)
     private Long idMcq;
+    @Column(name = "id_skill", nullable = false)
+    private Long idSkill;
 
     public Questions() {}
 
-    public Questions(String description, Long idMcq) {
+    public Questions(String description, Long idMcq, Long idSkill) {
         this.description = description;
         this.idMcq = idMcq;
+        this.idSkill = idSkill;
     }
 }
