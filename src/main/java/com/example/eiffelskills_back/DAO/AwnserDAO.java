7056dabb7d21eba257535b065376c8e172b16dd3
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-01-21 09:27:07 +0100

    Minor modifications

--- src/main/java/com/example/eiffelskills_back/DAO/AwnserDAO.java
+++ src/main/java/com/example/eiffelskills_back/DAO/AwnserDAO.java
@@ -4,7 +4,9 @@ import com.example.eiffelskills_back.models.Awnsers;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AwnserDAO extends JpaRepository<Awnsers,Long> {
     @Modifying
     @Query(nativeQuery = true, value = "UPDATE awnsers SET is_good=true WHERE id=:id")
