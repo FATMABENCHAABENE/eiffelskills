ca4f6fd55c224ce70fc559bf7d7975984cca0451
qentex4 <quentinlacombe464@yahoo.fr>
2025-01-24 15:09:42 +0100

    Add script to delete all tables.

---
+++ initdb/deleteAll.sql
@@ -0,0 +1,9 @@
+drop table if exists public.users cascade ;
+drop table if exists modules cascade ;
+drop table if exists skills cascade ;
+drop table if exists autoevaluations cascade ;
+drop table if exists mcq cascade ;
+drop table if exists questions cascade;
+drop table if exists awnsers cascade;
+drop table if exists contactmessage cascade;
+drop table if exists resources cascade;
