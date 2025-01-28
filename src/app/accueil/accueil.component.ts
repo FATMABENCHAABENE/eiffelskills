43db313a1155ba7b311e02b3d301d9b631440fd1
qentex4 <quentinlacombe464@yahoo.fr>
2025-01-28 13:09:09 +0100

    Quizz + détails

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -5,25 +5,29 @@ import { Service } from '../services/service.service';
 @Component({
   selector: 'accueil',
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.scss'
+  styleUrls: ['./accueil.component.scss']
 })
-
 export class AccueilComponent implements OnInit {
 
-candidateId: number = 0;
+  candidateId: number = 0;
+  isHelpMenuVisible: boolean = false; // État pour afficher ou masquer le menu
 
   constructor(private service: Service, private router: Router) { }
-  ngOnInit(): void {
-  }
 
-  selogin(value : number) : void {
+  ngOnInit(): void { }
+
+  selogin(value: number): void {
     this.service.setLoginIn(value);
     this.router.navigate(['/login']);
-  } 
+  }
 
   loadevalrecrut() {
     this.service.setId(this.candidateId);
     this.router.navigate(['/showeval']);
   }
-  
+
+  // Bascule l'état du menu d'aide
+  toggleHelpMenu(): void {
+    this.isHelpMenuVisible = !this.isHelpMenuVisible;
+  }
 }
