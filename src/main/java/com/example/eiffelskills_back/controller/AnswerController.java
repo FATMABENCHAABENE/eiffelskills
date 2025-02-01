2b373436a7e4058b4ef9500f7a786ff43213836d
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-02-01 10:10:28 +0100

    Minor orthograph modification.

--- src/main/java/com/example/eiffelskills_back/controller/AnswerController.java
+++ src/main/java/com/example/eiffelskills_back/controller/AnswerController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  * AwnserController :
- * Questions are inserted by teachers and managed through this controller. It is also used to check awnsers validity.
+ * Questions are inserted by teachers and managed through this controller. It is also used to check answers validity.
  */
 @CrossOrigin
 @RestController
@@ -20,9 +20,9 @@ public class AnswerController {
 
     /**
      * Method addByList
-     * @param list List(String) The list of awnsers description
-     * @param idQuestion The question awnsers are refered
-     * @return List(Awnsers) The list of created entries
+     * @param list List(String) The list of answers description
+     * @param idQuestion The question answers are refered
+     * @return List(Answers) The list of created entries
      */
     @PostMapping("/question/{idQuestion}")
     public List<Answers> addByList(@RequestBody List<String> list, @PathVariable Long idQuestion) {
@@ -30,53 +30,53 @@ public class AnswerController {
     }
 
     /**
-     * Method updateGoodAwnser
-     * @param id Long The ID of the awnser we want to make good
-     * Change the isGood attribute of the given awnser from false to true
+     * Method updateGoodAnswer
+     * @param id Long The ID of the answer we want to make good
+     * Change the isGood attribute of the given answer from false to true
      */
     @PostMapping("/updateGood")
-    public void updateGoodAwnser(@RequestBody Long id) {
+    public void updateGoodAnswer(@RequestBody Long id) {
         answerService.updateGoodAwnser(id);
     }
 
     /**
-     * Method getAllAwnsers
-     * @return The list of all awnser entries
+     * Method getAllAnswers
+     * @return The list of all answer entries
      */
     @GetMapping("")
-    public List<Answers> getAllAwnsers() {
+    public List<Answers> getAllAnswers() {
         return answerService.getAllAwnsers();
     }
 
     /**
-     * Method getAwnserByIdQuestion
-     * @param idQuestion Long The ID of the question we want to get awnsers
-     * @return All awnsers related to the given question
+     * Method getAnswerByIdQuestion
+     * @param idQuestion Long The ID of the question we want to get answers
+     * @return All answers related to the given question
      */
     @GetMapping("/question/{idQuestion}")
-    public List<Answers> getAwnserByIdQuestion(@PathVariable Long idQuestion) {
+    public List<Answers> getAnswerByIdQuestion(@PathVariable Long idQuestion) {
         System.out.println("je suis dans le back");
         return answerService.getAwnsersByIdQuestion(idQuestion);
     }
 
     /**
-     * Method checkAwnserById
-     * @param id Long ID of the awnser we want to check
-     * @return The attribute isGood of the given awnser
+     * Method checkAnswerById
+     * @param id Long ID of the answer we want to check
+     * @return The attribute isGood of the given answer
      */
     @GetMapping("/check/{id}")
-    public Boolean checkAwnserById(@PathVariable Long id) {
+    public Boolean checkAnswerById(@PathVariable Long id) {
         return answerService.checkAwnser(id);
     }
 
     /**
-     * Method checkGlobalAwnser
-     * @param idStudent <Long> Student who made awnsers
-     * @param idList <List(Long)> List of given awnsers
-     * Check if given awnsers are good and update evaluation in function of results
+     * Method checkGlobalAnswer
+     * @param idStudent <Long> Student who made answers
+     * @param idList <List(Long)> List of given answers
+     * Check if given answers are good and update evaluation in function of results
      */
     @PostMapping("/globalcheck/{idStudent}")
-    public void checkGlobalAwnser(@PathVariable Long idStudent, @RequestBody List<Long> idList) {
+    public void checkGlobalAnswer(@PathVariable Long idStudent, @RequestBody List<Long> idList) {
         answerService.checkListAwnser(idList, idStudent);
     }
 }
