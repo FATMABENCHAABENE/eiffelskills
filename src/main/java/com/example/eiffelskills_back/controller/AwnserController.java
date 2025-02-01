2167fce45930d69b022123d18cda802bcbc4e3ef
yureiiko <101016323+yureiiko@users.noreply.github.com>
2025-02-01 09:53:25 +0100

    Add javadoc for AwnserController.java .

--- src/main/java/com/example/eiffelskills_back/controller/AwnserController.java
+++ src/main/java/com/example/eiffelskills_back/controller/AwnserController.java
@@ -18,27 +18,52 @@ import java.util.List;
 public class AwnserController {
     private final AwnserService awnserService;
 
+    /**
+     * Method addByList
+     * @param list List(String) The list of awnsers description
+     * @param idQuestion The question awnsers are refered
+     * @return List(Awnsers) The list of created entries
+     */
     @PostMapping("/question/{idQuestion}")
     public List<Awnsers> addByList(@RequestBody List<String> list, @PathVariable Long idQuestion) {
         return awnserService.addByList(list, idQuestion);
     }
 
+    /**
+     * Method updateGoodAwnser
+     * @param id Long The ID of the awnser we want to make good
+     * Change the isGood attribute of the given awnser from false to true
+     */
     @PostMapping("/updateGood")
     public void updateGoodAwnser(@RequestBody Long id) {
         awnserService.updateGoodAwnser(id);
     }
 
+    /**
+     * Method getAllAwnsers
+     * @return The list of all awnser entries
+     */
     @GetMapping("")
     public List<Awnsers> getAllAwnsers() {
         return awnserService.getAllAwnsers();
     }
 
+    /**
+     * Method getAwnserByIdQuestion
+     * @param idQuestion Long The ID of the question we want to get awnsers
+     * @return All awnsers related to the given question
+     */
     @GetMapping("/question/{idQuestion}")
     public List<Awnsers> getAwnserByIdQuestion(@PathVariable Long idQuestion) {
         System.out.println("je suis dans le back");
         return awnserService.getAwnsersByIdQuestion(idQuestion);
     }
 
+    /**
+     * Method checkAwnserById
+     * @param id Long ID of the awnser we want to check
+     * @return The attribute isGood of the given awnser
+     */
     @GetMapping("/check/{id}")
     public Boolean checkAwnserById(@PathVariable Long id) {
         return awnserService.checkAwnser(id);
